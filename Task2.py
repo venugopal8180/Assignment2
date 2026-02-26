d43ef7c7d064a1a2d60ba1cf3feeb78aa125ec83
VENU GOPAL RAMIREDDY <venugopalrvg9@gmail.com>
2026-02-26 18:18:38 +0530

    Task 2 python script file added

---
+++ Task2.py
@@ -0,0 +1,14 @@
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+#sum is an variable to store a sum value of numbers 1 to 50 and it default value is 0
+
+sum=0
+
+#for loop to iterate over numbers from 1 to 50
+for i in range(1,51):
+    #Calculates the sum of all integers in this range.
+    sum+=i
+
+#Displays the final sum.
+print("The sum of numbers from 1 to 50 is: ",sum)
+ 
